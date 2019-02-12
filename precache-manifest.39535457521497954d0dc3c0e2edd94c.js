a752254cd0d42b84bbf7d6234e76c23bbfb5a36b
AiyazN25 <aiyaz.noor@mail.utoronto.ca>
2019-02-12 11:05:17 -0500

    Updates

---
+++ precache-manifest.39535457521497954d0dc3c0e2edd94c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a1df2310fa44a6f754",
+    "url": "/ygo-deckbuilder/static/js/runtime~main.61a1df23.js"
+  },
+  {
+    "revision": "134ec15a171909ae988d",
+    "url": "/ygo-deckbuilder/static/js/main.134ec15a.chunk.js"
+  },
+  {
+    "revision": "78997a5db8c2084d514f",
+    "url": "/ygo-deckbuilder/static/js/1.78997a5d.chunk.js"
+  },
+  {
+    "revision": "134ec15a171909ae988d",
+    "url": "/ygo-deckbuilder/static/css/main.7338d4a0.chunk.css"
+  },
+  {
+    "revision": "df9b7bcdcdf438b91e170a71350f09cf",
+    "url": "/ygo-deckbuilder/index.html"
+  }
+];
